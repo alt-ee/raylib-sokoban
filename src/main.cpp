090aee7d724253b91ba4122595b2263a6f203bd1
Alex <59831659+alt-ee@users.noreply.github.com>
2025-02-09 21:38:51 +0000

    feat:player movement and moving boxes

--- src/main.cpp
+++ src/main.cpp
@@ -27,26 +27,60 @@ using LevelArray =
     std::array<std::array<Tile, constants::max_width>, constants::max_height>;
 
 class Level {
+private:
+  bool MoveBox(int pos_x, int pos_y, int dir_x, int dir_y, Tile currentTile) {
+    int target_x{pos_x + dir_x};
+    int target_y{pos_y + dir_y};
+
+    Tile replaceTile;
+    if (currentTile == Tile::box_goal) {
+      replaceTile = Tile::goal;
+    } else {
+      replaceTile = Tile::none;
+    }
+
+    if (grid[target_y][target_x] == Tile::none) {
+      grid[target_y][target_x] = Tile::box;
+    } else if (grid[target_y][target_x] == Tile::goal) {
+      grid[target_y][target_x] = Tile::box_goal;
+    } else {
+      return false;
+    }
+
+    grid[pos_y][pos_x] = replaceTile;
+
+    return true;
+  }
+
 public:
   int height, width;
+  int player_x, player_y;
+  Tile player_replaceTile;
   LevelArray grid;
 
-  Level() : height{0}, width{0}, grid{Tile::none} {}
+  Level()
+      : height{0}, width{0}, player_x{0}, player_y{0},
+        player_replaceTile(Tile::none), grid{Tile::none} {}
 
   void AddRow(std::string row_string) {
     if (row_string.size() > width) {
       width = row_string.size();
     }
 
-    int i{0};
+    int x{0};
     for (char c : row_string) {
-      grid[height][i] = charmap.at(c);
-      i++;
+      grid[height][x] = charmap.at(c);
+      if (c == '@' || c == '+') {
+        player_x = x;
+        player_y = height;
+        if (c == '+') player_replaceTile = Tile::goal;
+      }
+      x++;
     }
     height++;
   }
 
-  void DrawLevel() {
+  void Draw() {
     for (int x = 0; x < width; x++) {
       for (int y = 0; y < height; y++) {
         DrawRectangle(x * constants::tile_width, y * constants::tile_height,
@@ -54,6 +88,38 @@ public:
                       tilemap.at(grid[y][x]));
       }
     }
+  }
+
+  void MovePlayer(int dir_x, int dir_y) {
+    int target_x { player_x + dir_x };
+    int target_y { player_y + dir_y };
+    Tile target_tile { grid[target_y][target_x] };
+    
+    if (target_tile == Tile::box || target_tile == Tile::box_goal) {
+      if (!MoveBox(target_x, target_y, dir_x, dir_y, target_tile)) return;
+    } else if (target_tile == Tile::wall) {
+      return;
+    }
+
+    // The tile we are replacing the target destination with
+    // It will either be just the player if the destination is empty
+    // or a player-on-goal tile if the destiation is the goal
+    Tile target_replaceTile { Tile::player };
+    if (target_tile == Tile::goal || target_tile == Tile::box_goal) {
+      target_replaceTile = Tile::player_goal;
+    }
+      
+
+    grid[player_y][player_x] = player_replaceTile;
+
+    // If a box was pushed above, the value of target_tile is no longer valid,
+    // so we need to re-access the target destination to set player_replaceTile
+    player_replaceTile = grid[target_y][target_x];
+    grid[target_y][target_x] = target_replaceTile;
+    player_x = target_x;
+    player_y = target_y;
+
+    return;
   }
 };
 
@@ -75,8 +141,19 @@ int main() {
   SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
+
+    if (IsKeyPressed(KEY_RIGHT)) {
+      level.MovePlayer(1, 0);
+    } else if (IsKeyPressed(KEY_LEFT)) {
+      level.MovePlayer(-1, 0);
+    } else if (IsKeyPressed(KEY_DOWN)) {
+      level.MovePlayer(0, 1);
+    } else if (IsKeyPressed(KEY_UP)) {
+      level.MovePlayer(0, -1);
+    }
+
     BeginDrawing();
-    level.DrawLevel();
+    level.Draw();
     EndDrawing();
   }
 
